3b357cef810a9e99eb1bf60747df552ae9047f34
Napster Long <huangzhaolong1234@163.com>
2024-02-07 00:56:23 +0800

    init flask

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import request
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["POST"])
+def hello_world():
+    data = request.json()
+    rsp = {}
+    if "url_verification" == body.get("type", ""):
+        challenge = data.get("challenge")
+        rsp = {"challenge": challenge}
+    else:
+        pass
+    return rsp
