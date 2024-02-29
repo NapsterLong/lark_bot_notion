db0f726e5e5409523c248711a2b7c9ca98ae836a
Napster Long <huangzhaolong1234@163.com>
2024-02-29 22:49:52 +0800

    fix log

--- app.py
+++ app.py
@@ -2,6 +2,23 @@ import os
 import sys
 sys.path.append(os.path.dirname(__file__))
 import time
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
 from flask import Flask
 from flask import request
